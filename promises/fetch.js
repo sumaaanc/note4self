dcb0231db95df9d833d40608dc13fbaf59692398
moonlitskies <smnch07@gmail.com>
2023-11-27 09:37:26 +0545

    async

--- promises/fetch.js
+++ promises/fetch.js
@@ -1,9 +1,29 @@
 //fetch api returns promise
 const URL = 'https://jsonplaceholder.typicode.com/users'
 
-fetch(URL)  //returns promise
-.then((response)=> {
-    return response.json()
-})
-.then((data)=> {console.log(data)})
-.catch((error)=> {console.log(error)})
+// fetch(URL)  //returns promise
+// .then((response)=> {
+//     return response.json()  //string ma aako response lai json ma convert gareko 
+// })
+// .then((data)=> {console.log(data)})
+// .catch((error)=> {console.log(error)})
+
+
+//same thing from using async/await 
+
+async function getAllUser(){
+    try {
+        const response = await fetch(URL)
+        const data = await response.json()
+        const allUserName = data.map((item)=>{
+            return (`ID:${item.id} Username: ${item.username}`)
+        })
+        console.log(allUserName)
+        
+    } catch (error) {
+        console.log('Error: ' + error)
+    }
+    
+
+}
+getAllUser();
